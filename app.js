bd45108f8ab077e8b607b68eadad026e90216d5d
Azazel <ildarhanovazat551@gmail.com>
2025-12-24 20:36:20 +0300

    init Node.js project

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-nothing
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Привет, Азат! 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
